e9d64e4e0d46feea4bc8cfd84233919ff7190067
jonas <jonasmeister@gmail.com>
2022-12-06 18:38:22 +0100

    new commit

--- src/PanelStreamingUIAll.java
+++ src/PanelStreamingUIAll.java
@@ -11,9 +11,7 @@ public class PanelStreamingUIAll extends JPanel {
 
         stream = new Stream();
         this.addNumberButtons();
-
-
-
+        
     }
 
     private void addNumberButtons() {
