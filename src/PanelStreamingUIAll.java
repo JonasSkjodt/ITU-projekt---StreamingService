0497094631aceb3fd8e39f8dbbbddb2f0d67f3f7
jonas <jonasmeister@gmail.com>
2022-12-06 21:55:33 +0100

    new animation hover effect

--- src/PanelStreamingUIAll.java
+++ src/PanelStreamingUIAll.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelStreamingUIAll extends JPanel {
     private Stream stream;
@@ -10,10 +12,13 @@ public class PanelStreamingUIAll extends JPanel {
         this.addImageButtons();
     }
 
+    /**
+     * movie and series button with image
+     * TODO: Get better resolution images
+     */
     private void addImageButtons() {
         for (Media m : stream.getMediaList()) {
             //this.add(new JLabel(m.getImageMedia()));
-
             //String label = m.getName();
             JButton mediaButton = new JButton();
             try {
@@ -25,13 +30,34 @@ public class PanelStreamingUIAll extends JPanel {
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
+
+            //style the button a bit
             mediaButton.setForeground(Color.white);
             mediaButton.setBorder(new EmptyBorder(15, 15, 15, 15));
             //remove standard styling
             mediaButton.setBorderPainted(false);
             mediaButton.setFocusPainted(false);
             mediaButton.setContentAreaFilled(false);
+
+            //hover animation effect, insert the new image on mouseEntered, and the old image on mouseExited
+            ImageIcon hoverImage2 = new ImageIcon("data/img/play.png");
+            mediaButton.addMouseListener(new MouseAdapter() {
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    mediaButton.setIcon(hoverImage2);
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    ImageIcon imageIcon = m.getImageMedia(); //load the images
+                    mediaButton.setIcon(imageIcon);
+                }
+            });
+
+            //add the mediaButton to the panel
             this.add(mediaButton);
         }
     }
+
 }
